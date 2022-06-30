c7681267095737def0176bdf1c4e798257947f47
maxpowernz <helmoreman@gmail.com>
2022-06-30 16:57:24 +1200

    color

--- src/components/ComponentWrapper.tsx
+++ src/components/ComponentWrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function ComponentWrapper({ children }: React.PropsWithChildren) {
   return (
-    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px", backgroundColor: "lightblue" }}>
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px", backgroundColor: "lightgray" }}>
       {children}
     </div>
   );
